a783d38ddca3b914e34da7bac5feb93ff168e5ca
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-01 17:20:15 -0500

    Add solution

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from './Links';
 
-function About() {
+function About({user}) {
+  const{links, bio} = user
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{bio? bio:null }</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links linkedin={links.linkedin} github={links.github}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+
+
+function Links({linkedin, github}){
+
+
+    return(
+        <div>
+            <h3>Links</h3>
+            <a href={github}>https://github.com/Liza</a>
+            <a href={linkedin}>https://www.linkedin.com/in/liza</a>
+        </div>
+    )
+}
+
+export default Links;
